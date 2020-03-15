7a9689fa38ef746a679c080497821d6d17e915d0
eRy-sk <ride.poke@gmail.com>
2020-03-16 00:09:13 +0900

    ヘッダーを実装

--- src/App.js
+++ src/App.js
@@ -1,27 +1,35 @@
 import React from 'react';
 import logo from './logo.svg';
-import 'bootstrap'
-import 'bootstrap/scss/bootstrap.scss'
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+          <nav className='navbar navbar-expand-lg navbar-light bg-light'>
+              <a className='navbar-brand' href='#'>MyPage</a>
+              <button type='button' className='navbar-toggler' data-toggle="collapse" data-target="#Navber" aria-controls="Navber" aria-expanded="false" aria-label="responsive-navbar" >
+                  <span className="navbar-toggler-icon"></span>
+              </button>
+              <div className="collapse navbar-collapse" id="Navber">
+                  <ul className="navbar-nav mr-auto mt-2 mt-lg-0">
+                      <li className='nav-item active'>
+                          <a className="nav-link" href="#">Profile</a>
+                      </li>
+                      <li className='nav-item'>
+                          <a className="nav-link" href="#">History</a>
+                      </li>
+                      <li className='nav-item'>
+                          <a className="nav-link" href="#">Portfolio</a>
+                      </li>
+                  </ul>
+                  <div className='navbar-nav'>
+                      <div className='nav-item'>
+                          <a className="nav-link" href="#">Contact</a>
+                      </div>
+                  </div>
+              </div>
+          </nav>
       </header>
-    </div>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'bootstrap'
+import 'bootstrap/scss/bootstrap.scss'
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
